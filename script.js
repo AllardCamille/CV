be19037b7dfb260ae6dbb41b09b99c7a8348346b
AllardCamille <allardcam31@gmail.com>
2025-03-26 14:59:30 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
 const icone = document.getElementById("icone");
 const sommaire = document.getElementById("sommaire");
 
-icone.addEventListener("click", () => {
+icone.addEventListener("click", (event) => {
     sommaire.classList.toggle("active");
     event.stopPropagation();
 });
 
+// Fermer sommaire si on clique ailleurs
 document.addEventListener("click", (event) => {
     if (!sommaire.contains(event.target) && !icone.contains(event.target)) {
         sommaire.classList.remove("active");
     }
 });
+
+// Fermer sommaire si on clique dessus
+sommaire.addEventListener("click", () => {
+    sommaire.classList.remove("active");
+});
+
